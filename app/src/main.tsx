af9dc537868becb1cb4fba91fb539c07c44c715b
Jeremy Foster <jeremy.foster@fosol.ca>
2023-04-17 16:44:13 -0700

    Add styled components

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,15 @@
+import './index.scss'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './index.scss'
+import { ThemeProvider } from 'styled-components'
+import css from './css/_variables.module.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={{ css }}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
