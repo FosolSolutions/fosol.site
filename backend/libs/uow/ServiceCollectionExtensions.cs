a47efa421c4c717a85d43b49baa963ab0fc1f3a5
Jeremy Foster <jeremy.foster@fosol.ca>
2023-05-10 07:47:26 -0700

    Fix db script

--- backend/libs/uow/ServiceCollectionExtensions.cs
+++ backend/libs/uow/ServiceCollectionExtensions.cs
@@ -26,8 +26,8 @@ public static class ServiceCollectionExtensions
     // Find all the configuration classes.
     var assembly = typeof(BaseService).Assembly;
     var type = typeof(IBaseService);
-    var tnoServiceTypes = assembly.GetTypes().Where(t => !t.IsAbstract && t.IsClass && t.GetInterfaces().Any(i => i.Name.Equals(type.Name)));
-    foreach (var serviceType in tnoServiceTypes)
+    var serviceTypes = assembly.GetTypes().Where(t => !t.IsAbstract && t.IsClass && t.GetInterfaces().Any(i => i.Name.Equals(type.Name)));
+    foreach (var serviceType in serviceTypes)
     {
       var sInterface = serviceType.GetInterface($"I{serviceType.Name}") ?? throw new InvalidOperationException($"Service type '{serviceType.Name}' is missing its interface.");
       services.AddScoped(sInterface, serviceType);
